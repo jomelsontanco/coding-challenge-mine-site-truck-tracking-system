080010bbade491856bd278a1271bb96afb0c49ea
Jomelson Co <jomelsontanco@gmail.com>
2025-11-17 21:56:33 +1000

    Add map component to root

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,5 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { App } from './app';
+import { By } from '@angular/platform-browser';
+import { MapComponent } from './components/map-component/map-component';
 
 describe('App', () => {
   beforeEach(async () => {
@@ -14,10 +16,10 @@ describe('App', () => {
     expect(app).toBeTruthy();
   });
 
-  it('should render title', () => {
+  it('should render map component', () => {
     const fixture = TestBed.createComponent(App);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, coding-challenge-mine-site-truck-tracking-system');
+    const childDebugElement = fixture.debugElement.query(By.directive(MapComponent));
+    expect(childDebugElement).not.toBeNull();
   });
 });
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MapComponent } from './components/map-component/map-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [MapComponent],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
-export class App {
-  protected readonly title = signal('coding-challenge-mine-site-truck-tracking-system');
-}
+export class App {}
